c1cc98bba76207b70c0b819ed7d2d1b0852b1104
LAOI020 <90497490+LAOI020@users.noreply.github.com>
2022-08-08 13:19:28 -0500

    Add files via upload

---
+++ reporte_zpl/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'reportes ZPL',
+    'author': "Isaac",
+    'category': 'Other',
+    'version': '1.0',
+    'application': False,
+    'depends': [
+     'base','stock'
+    ],
+    'data': [
+        'reports/stock_picking_view.xml',
+    ]
+}
