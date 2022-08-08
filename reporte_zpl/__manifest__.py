ba5335b4e08acd109434d1d70ee923e35b3cf61e
LAOI020 <90497490+LAOI020@users.noreply.github.com>
2022-08-08 14:38:29 -0500

    Add files via upload

---
+++ reporte_zpl/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'reportes ZPL',
+    'author': "Isaac",
+    'category': 'Other',
+    'version': '1.0',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'depends': [
+     'base','stock'
+    ],
+    'data': [
+        'views/stock_picking_view.xml',
+    ]
+}
---
+++ reporte_zpl/models/stock_picking.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+    
+    def send_zpl_report(self):
+        _logger.info("si se ejecutaaaa")
+        _logger.info(self)
+        _logger.info("si se ejecutaaaa")
+        
